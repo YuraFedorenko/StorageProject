7701fd4a37196cc8ff30dd34c3016f4f1ec80a86
YuraFedorenko <arhimedyura@gmail.com>
2018-09-25 11:42:01 +0300

    File Entity class

--- Storage/src/main/java/com/transfer/Storage/model/File.java
+++ Storage/src/main/java/com/transfer/Storage/model/File.java
@@ -1,5 +1,32 @@
 package com.transfer.Storage.model;
 
-public class File {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import lombok.Data;
+
+@Data
+@Entity
+public class File {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@Column(name="file_name")
+	private String name;
+	
+	@Column(name="file_format")
+	private String format;
+	
+	@Column(name="file_size")
+	private long size;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Storage storage;
 }
