733c1c87bd128fe9e8344e1900f532a002acbb64
sergio <arhimedyura@gmail.com>
2018-09-25 12:50:53 +0300

    init commit

--- Storage/src/main/java/com/transfer/Storage/model/Storage.java
+++ Storage/src/main/java/com/transfer/Storage/model/Storage.java
@@ -1,5 +1,14 @@
 package com.transfer.Storage.model;
 
+
+
 public class Storage {
+	
 private long id;
+private String[] formatsSupported;
+private String storageCountry;
+private long storageSize;
+
+
+
 }
